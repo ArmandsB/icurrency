524c95cc7a199985938e93724c5e8a818a11d058
Armands Baurovskis <baurovskis.armands@gmail.com>
2019-04-29 12:56:16 +0300

    App icon

--- iCurrencyTests/Currency/ViewModels/CurrencyCellViewModelTests.swift
+++ iCurrencyTests/Currency/ViewModels/CurrencyCellViewModelTests.swift
@@ -6,8 +6,34 @@
 //  Copyright © 2019 iOSCoder. All rights reserved.
 //
 
+import RxBlocking
+import RxCocoa
+import RxSwift
+import RxTest
 import XCTest
+@testable import iCurrency
 
 class CurrencyCellViewModelTests: XCTestCase {
+
+  let disposeBag = DisposeBag()
+  let activeCurrencyProperty: BehaviorRelay<CurrencyCellViewModel?> = .init(value: nil)
+  var activeCurrencyObservable: Observable<CurrencyCellViewModel?>!
+  var viewModel: CurrencyCellViewModelType!
+  var scheduler: TestScheduler!
+  
+  override func setUp() {
+    super.setUp()
+    scheduler = TestScheduler(initialClock: 0)
+    activeCurrencyObservable = activeCurrencyProperty.asObservable()
+    let currency = Currency(name: "EUR", rate: 1.1234)
+    viewModel = CurrencyCellViewModel(currency: currency,
+                                      activeCurrencyObservable: activeCurrencyObservable)
+  }
+}
+
+
+// MARK: Inputs
+
+extension CurrencyCellViewModelTests {
 
 }
